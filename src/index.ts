66bac9e9eba0fd1ad4552491501155afc05a7a4a
Lloyd Janse van Rensburg <thefullstackjunkie@gmail.com>
2021-10-16 19:47:19 +0200

    final project one

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi there');
+import { Map } from './Map';
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+const map = new Map('mapDiv');
+
+map.addMarker(user);
+map.addMarker(company);
