b65aea7499f6397cac62f583563173bcb82fe207
Lloyd Janse van Rensburg <thefullstackjunkie@gmail.com>
2021-10-16 20:44:47 +0200

    bubble sort implemented

--- build/index.js
+++ build/index.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log(123);
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Sorter = void 0;
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                var left = this.collection[j];
+                var right = this.collection[j + 1];
+                if (left > right) {
+                    var temp = left;
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+exports.Sorter = Sorter;
+var sorter = new Sorter([10, 3, -5, 0]);
+console.log(sorter.collection);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log(123);
+export class Sorter {
+  collection: number[];
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        let left = this.collection[j];
+        let right = this.collection[j + 1];
+
+        if (left > right) {
+          let temp = left;
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+
+console.log(sorter.collection);
+sorter.sort();
+console.log(sorter.collection);
