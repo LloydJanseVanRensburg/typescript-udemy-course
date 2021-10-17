34e20a3a9fcfe2abe1d90bf2ab59ed49afa6c2ec
Lloyd Janse van Rensburg <thefullstackjunkie@gmail.com>
2021-10-17 19:04:16 +0200

    data analysed from the csv file

---
+++ build/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var matches = fs_1.default
+    .readFileSync('football.csv', {
+    encoding: 'utf-8',
+})
+    .split('\n')
+    .map(function (row) {
+    return row.split(',');
+});
+var manUnitedWins = 0;
+/**
+ * 0 Date
+ * 1 Home Team
+ * 2 Away Team
+ * 3 Home Score
+ * 4 Away Score
+ * 5 Winner H || A
+ * 6 Referre
+ */
+for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
+    var match = matches_1[_i];
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+}
+console.log("Man United won " + manUnitedWins + " games");
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+let manUnitedWins = 0;
+
+/**
+ * 0 Date
+ * 1 Home Team
+ * 2 Away Team
+ * 3 Home Score
+ * 4 Away Score
+ * 5 Winner H || A
+ * 6 Referre
+ */
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
